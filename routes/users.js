27f96482cc2516e0d33ade324daa122f5f4b6c45
horaceheaven <hheaven@medullan.com>
2015-08-29 11:42:19 -0400

    created user a/c creations view

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/create', function(req, res, next){
+	var vm = {
+		title: 'Create an account'
+	};
+
+	res.render('users/create', vm);	
+});
+
 module.exports = router;
